e69e8a5da1cc7f63aab7e78b1d38d63a203b9b10
F8ASB <f8asb@hotmail.fr>
2022-09-17 17:42:50 +0200

    Update ircddb_status.py

--- ircddb_status.py
+++ ircddb_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#GET Informations about your repeatee on IRCDDB Database
+#GET Informations about your repeater on IRCDDB Database
 #
 #F8ASB 2022 - F8ASB.COM
 #
